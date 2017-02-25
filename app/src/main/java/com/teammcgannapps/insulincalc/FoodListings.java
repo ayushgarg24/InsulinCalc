7e9c284fc25564f0939dcb71f9293fd87d49d99e
cescott <chuckescott@comcast.net>
2017-02-25 13:44:21 -0800

    added scrollview
    nice my boiii

--- app/src/main/java/com/teammcgannapps/insulincalc/FoodListings.java
+++ app/src/main/java/com/teammcgannapps/insulincalc/FoodListings.java
@@ -1,11 +1,13 @@
 package com.teammcgannapps.insulincalc;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class FoodListings extends AppCompatActivity {
 
@@ -14,6 +16,17 @@ public class FoodListings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_listings);
 
+        // Font path
+        String fontPath = "fonts/junegull.ttf";
+
+        // text view label
+        TextView txtGhost = (TextView) findViewById(R.id.food_title);
+
+        // Loading Font Face
+        Typeface tf = Typeface.createFromAsset(getAssets(), fontPath);
+
+        // Applying font
+        txtGhost.setTypeface(tf);
     }
 
 }
