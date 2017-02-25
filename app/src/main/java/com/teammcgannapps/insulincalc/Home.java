938d0dbd4cd20a95d11e25122e353b9f32647fea
Ayush Garg <ayushgarg1551@gmail.com>
2017-02-25 12:00:27 -0800

    added files

---
+++ app/src/main/java/com/teammcgannapps/insulincalc/Home.java
@@ -0,0 +1,16 @@
+package com.teammcgannapps.insulincalc;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+
+}
