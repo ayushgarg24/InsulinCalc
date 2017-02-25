9be139d997dcc90f9e5e610e910d6a7e6a192c07
Ayush Garg <ayushgarg1551@gmail.com>
2017-02-25 12:21:41 -0800

    GHOST BITCH
    hey

--- app/src/main/java/com/teammcgannapps/insulincalc/Home.java
+++ app/src/main/java/com/teammcgannapps/insulincalc/Home.java
@@ -3,6 +3,10 @@ package com.teammcgannapps.insulincalc;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
 
@@ -10,7 +14,18 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        // Font path
+        String fontPath = "fonts/Face Your Fears.ttf";
 
+        // text view label
+        TextView txtGhost = (TextView) findViewById(R.id.ghost);
+
+        // Loading Font Face
+        Typeface tf = Typeface.createFromAsset(getAssets(), fontPath);
+
+        // Applying font
+        txtGhost.setTypeface(tf);
     }
+
 
 }
