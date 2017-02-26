dc91aeb233150992d606ff48293dc202b44c8b4d
cescott <chuckescott@comcast.net>
2017-02-25 16:09:35 -0800

    stuff

--- app/src/main/java/com/teammcgannapps/insulincalc/Settings.java
+++ app/src/main/java/com/teammcgannapps/insulincalc/Settings.java
@@ -1,5 +1,7 @@
 package com.teammcgannapps.insulincalc;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -7,7 +9,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Arrays;
 
 public class Settings extends AppCompatActivity {
 
@@ -66,6 +72,28 @@ public class Settings extends AppCompatActivity {
         txtGhost7.setTypeface(tf2);
 
 
+    }
+
+    public void confirmSettings(View view) {
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("myPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putInt("userSettings_size", 3);
+        EditText e1 = (EditText) findViewById(R.id.editText1);
+        EditText e2 = (EditText) findViewById(R.id.editText2);
+        EditText e3 = (EditText) findViewById(R.id.editText3);
+        String[] values = {e1.getText().toString(), e2.getText().toString(), e3.getText().toString()};
+        for (int i=0; i < 3;i++) {
+            editor.putString("userSettings_" + i, values[i]);
+        }
+        editor.commit();
+
+        Context context = getApplicationContext();
+        CharSequence text = "Settings Saved";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
 }
