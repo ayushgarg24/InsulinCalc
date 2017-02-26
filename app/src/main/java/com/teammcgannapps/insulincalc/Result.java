c3ef2795f89df767f3eff870e717f7356be52abc
Ayush Garg <ayushgarg1551@gmail.com>
2017-02-25 16:42:29 -0800

    some things??
    idk

---
+++ app/src/main/java/com/teammcgannapps/insulincalc/Result.java
@@ -0,0 +1,25 @@
+package com.teammcgannapps.insulincalc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+    }
+    public void startResult(View view)
+    {
+        Intent intent = new Intent (this, Result.class);
+        startActivity(intent);
+    }
+
+}
